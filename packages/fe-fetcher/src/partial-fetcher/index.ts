0bf9271c06a5b7b7e5c87894f8725207c61a233b
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-04-12 12:33:20 +0300

    add scope

--- packages/fe-fetcher/src/partial-fetcher/index.ts
+++ packages/fe-fetcher/src/partial-fetcher/index.ts
@@ -1,5 +1,4 @@
 export * from './partial-fetcher';
 export * from './fetch-result';
 export * from './fetch-action';
-export * from './indicators-partial-fetcher.service';
 export * from './consts';
